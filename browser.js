7b9e4bf6286e0eac2d109ffbaae021c582426bac
Jan Bölsche <regular.gonzales@googlemail.com>
2020-09-24 10:59:57 +0200

    reconnect if connection was dropped

---
+++ browser.js
@@ -0,0 +1,107 @@
+const ssbClient = require('ssb-client')
+const multicb = require('multicb')
+const Reconnect = require('pull-reconnect')
+const wrapAPI = require('wrap-muxrpc-api')
+const debug = require('debug')('tre-client')
+
+function client(keys, caps, remote, cb) {
+  function _client(keys, caps, remote, manifest, cb) {
+    ssbClient(keys, {caps, remote, manifest}, cb)
+  }
+
+  _client(keys, caps, remote, {manifest: 'sync'}, (err, ssb) => {
+    if (err) return cb(err)
+    ssb.manifest( (err, manifest) => {
+      if (err) return cb(err)
+      debug('received manifest')
+      
+      let currSSB = null
+      let reconnects = 0
+
+      function connect(reconnect) {
+        debug('connecting ...')
+        _client(keys, caps, remote, manifest, (err, ssb) => {
+          if (err) {
+            debug('connecting failed: %s', err.message)
+            return reconnect(err)
+          }
+          ssb.on('closed', ()=>{
+            debug('connection closed')
+            reconnect(new Error('sbot connection lost'))
+          })
+          debug('connected')
+          currSSB = ssb
+          if (0 == reconnects++) {
+            // first time successfully connected
+            cb(null, wrap(ssb, reconnect))
+          }
+          reconnect(null)
+        })
+      }
+
+      function wrap(ssb, reconnect) {
+        return wrapAPI(ssb, manifest, function(fn, type, path) {
+          if (!reconnect[type]) {
+            const msg = `${type} not supported by pull-reconnect (${path.join(',')})`
+            console.error(msg)
+            throw Error(msg)
+          }
+          return reconnect[type](function() {
+            fn = traverse(currSSB).get(path)
+            return fn.apply(this, Array.from(arguments))
+          })
+        })
+      }
+      
+      Reconnect(connect)
+    })
+  })
+}
+
+function get(url, opts, cb) {
+  fetch(url, opts)
+    .then(r => r.json())
+    .then(j => cb(null, j))
+    .catch( e => cb(e))
+}
+
+module.exports.client = function(cb) {
+  const keys = loadKeys('tre-keypair')
+  const done = multicb({pluck:1, spread: true})
+
+  get(document.location.origin + '/.trerc?dontcache=' + Date.now(), {credentials: 'same-origin'}, done())
+  get(document.location.origin + '/.tre/ws-address', {}, done())
+  done( (err, config, remote) => {
+    if (err) return cb(err)
+    //console.log('config', config)
+    //console.log('remote', remote)
+    client(keys, config.caps, remote, (err, ssb) => {
+      if (err) return cb(err)
+      ssb.whoami( (err, feed) => {
+        if (err) return cb(err)
+        console.log('pub key', feed.id)
+
+        // add ssb.revisions.patch
+        if (ssb.revisions) {
+          ssb.revisions.patch = function(oldRev, patchFunc, cb) {
+            ssb.get(oldRev, (err, msg)=>{
+              if (err) return cb(err)
+              let content = patchFunc(msg.content)
+              if (content == undefined) content = msg.content // patched in-place
+              content.revisionBranch = oldRev
+              content.revisionRoot = content.revisionRoot || oldRev
+              ssb.publish(content, cb)
+            })
+          }
+        }
+        cb(null, ssb, config)
+      }) 
+    })
+  })
+}
+
+function loadKeys(name) {
+  const json = sessionStorage[name] || localStorage[name]
+  return JSON.parse(json)
+}
+
--- index.js
+++ index.js
@@ -1,70 +1,6 @@
-const ssbClient = require('ssb-client')
-const multicb = require('multicb')
-const pull = require('pull-stream')
+const fs = require('fs')
+const url = require('url')
 
-function client(keys, caps, remote, cb) {
-  function _client(keys, caps, remote, manifest, cb) {
-    ssbClient(keys, {caps, remote, manifest}, cb)
-  }
-
-  _client(keys, caps, remote, {manifest: 'sync'}, (err, ssb) => {
-    if (err) return cb(err)
-    ssb.manifest( (err, manifest) => {
-      if (err) return cb(err)
-      //console.log('manifest', manifest)
-      _client(keys, caps, remote, manifest, cb)
-    })
-  })
-}
-
-function get(url, opts, cb) {
-  fetch(url, opts)
-    .then(r => r.json())
-    .then(j => cb(null, j))
-    .catch( e => cb(e))
-}
-
-module.exports.client = function(cb) {
-  const keys = loadKeys('tre-keypair')
-  const done = multicb({pluck:1, spread: true})
-
-  get(document.location.origin + '/.trerc?dontcache=' + Date.now(), {credentials: 'same-origin'}, done())
-  get(document.location.origin + '/.tre/ws-address', {}, done())
-  done( (err, config, remote) => {
-    if (err) return cb(err)
-    //console.log('config', config)
-    //console.log('remote', remote)
-    client(keys, config.caps, remote, (err, ssb) => {
-      if (err) return cb(err)
-      ssb.whoami( (err, feed) => {
-        if (err) return cb(err)
-        console.log('pub key', feed.id)
-
-        // add ssb.revisions.patch
-        if (ssb.revisions) {
-          ssb.revisions.patch = function(oldRev, patchFunc, cb) {
-            ssb.get(oldRev, (err, msg)=>{
-              if (err) return cb(err)
-              let content = patchFunc(msg.content)
-              if (content == undefined) content = msg.content // patched in-place
-              content.revisionBranch = oldRev
-              content.revisionRoot = content.revisionRoot || oldRev
-              ssb.publish(content, cb)
-            })
-          }
-        }
-        cb(null, ssb, config)
-      }) 
-    })
-  })
-}
-
-function loadKeys(name) {
-  const json = sessionStorage[name] || localStorage[name]
-  return JSON.parse(json)
-}
-
-// sbot plugin interface
 exports.name = 'tre-client'
 exports.version = require('./package.json').version
 exports.manifest = {
@@ -72,9 +8,6 @@ exports.manifest = {
 exports.init = function (ssb, config) {
   const ws_address = JSON.stringify(ssb.ws.getAddress())
   const data_path = config.path 
-  const r = require
-  const fs = r('fs')
-  const url = r('url')
   fs.writeFileSync(data_path + '/ws-address', ws_address, 'utf8')
   
   ssb.ws.use(function (req, res, next) {
