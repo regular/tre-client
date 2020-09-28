2c4e6b60be9e417227ce06f467b77aff4d4660de
Jan Bölsche <regular.gonzales@googlemail.com>
2020-09-28 10:40:03 +0200

    Add one more case for an error that is worth a retry

--- worth-a-retry.js
+++ worth-a-retry.js
@@ -3,5 +3,7 @@ module.exports = function(err) {
   const {message} = err
   if (message == 'unexpected end of parent stream') return true
   if (message == 'unexpected hangup') return true
+  if (message.match(/stream ended with:(\d+) but wanted:34/)) return true
+
   return false
 }
